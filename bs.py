7246ca8120857f85e1c4f1f9d4c67c45da7a130c
skar777123 <163728641+skar777123@users.noreply.github.com>
2024-04-07 20:51:13 +0530

    bs.py

--- bs.py
+++ bs.py
@@ -6,13 +6,15 @@ K = 2000 # Strike Price
 T = 0.5  # Time to Maturity in Years
 r = 0.1  # Risk-free Rate
 sd= 0.2  # Standard Deviation / volatility
-Mp= 200  # Market Price
+Mp= (S/10)  # Market Price
 
 d1 = (m.log(S/K)+(r + 0.5 * sd**2)*T)/(sd* m.sqrt(T))
 d2 = d1 - (sd* m.sqrt(T))
 
 call = S * norm.cdf(d1) - K * m.exp(-r * T) * norm.cdf(d2)
 put = K * m.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+print()
+print("Market price of slots:",Mp)
 print()
 print("The value of d1 : ", round(d1,4))
 print("The value of d2 : ", round(d2,4))
